473bf5c35474b523b1fb80cdf7aa1d17ffa51572
Rowan Miller <rmiller85@charlottesvilleschools.org>
2020-10-26 22:19:14 +0000

    created dice roller.

---
+++ Python/dice_roller.py
@@ -0,0 +1,21 @@
+# Automatic Dice Roller
+# Written by Rowan Miller
+
+from random import randint
+
+global roll # creates a variable for the result of the roll to be stored in
+
+print("Automatic Dice Roller")
+
+print("Press Enter to roll a d6. Type d20, then Enter to roll a d20. Press x, then Enter to quit.")
+
+while 1 == 1:
+	input = input() # asks the user for input
+	if input == "x":
+		break # stops the loop if the user enters x
+	elif input == "d20":
+		roll = randint(1,20) # takes a random number between 1 & 20 and saves it as the result of the roll.
+	else:
+		roll = randint(1,6) # takes a random number between 1 & 6 and saves it as the result of the roll.
+	print roll
+
