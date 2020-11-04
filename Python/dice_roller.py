cea969c99c8a03baf80530e5cf8fbe2cfe66d59c
rmiller85 <55250364+rmiller85@users.noreply.github.com>
2020-11-04 13:24:52 -0500

    changed name of input variable so it could be a variable

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -10,10 +10,10 @@ print("Automatic Dice Roller")
 print("Press Enter to roll a d6. Type d20, then Enter to roll a d20. Press x, then Enter to quit.")
 
 while 1 == 1:
-	input = input() # asks the user for input
-	if input == "x":
+	my_input = input() # asks the user for input
+	if my_input == "x":
 		break # stops the loop if the user enters x
-	elif input == "d20":
+	elif my_input == "d20":
 		roll = randint(1,20) # takes a random number between 1 & 20 and saves it as the result of the roll.
 	else:
 		roll = randint(1,6) # takes a random number between 1 & 6 and saves it as the result of the roll.
